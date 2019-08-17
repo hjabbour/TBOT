9a920504c79c3068b40960d49022eca1b8296d50
GitTask <gittask@notused.com>
2019-08-17 09:41:50 -0300

    more tests

--- telegram_bot/utils/dbConnection.py
+++ telegram_bot/utils/dbConnection.py
@@ -2,10 +2,13 @@ from peewee import SqliteDatabase
 import os
 
 class DBConnection:
-    db = SqliteDatabase(os.getenv("db_name") or "telegram-bot.db")
+    db = None
 
     @staticmethod
     def getDB():
+        if(DBConnection.db == None):
+            DBConnection.db = SqliteDatabase(os.getenv("db_name") or "telegram-bot.db")
+
         if DBConnection.db.is_closed():
             DBConnection.db.connect()
             
--- tests/test_peewee.py
+++ tests/test_peewee.py
@@ -1,12 +1,39 @@
 import unittest
 
+import os
+
+db_file = "telegram_bot_test.db"
+
+#Hack =(
+os.environ["db_name"] = db_file
+
 from telegram_bot.models.user import UserModel
+from telegram_bot.utils.dbutils import DBUtil
+from telegram_bot.utils.dbConnection import DBConnection
 
 class TestPeewee(unittest.TestCase):
 
-    def test_user_model(self):
+    
+
+    def test_1_init_db(self):
+        DBUtil.initDB()
+
+    def test_2_get_db(self):
+        db = DBConnection.getDB()
+        self.assertIsNotNone(db)
+
+    def test_3_create_user_model(self):
+        newUser = UserModel.create(telegramUserId = 1, firstName="Test", lastName="Last", userName="usertest")
+        newUser.save()
+
+    def test_4_user_model_get(self):
         user = UserModel.get_or_none()
         self.assertIsNotNone(user)
+    
+    @classmethod
+    def tearDownClass(cls):
+        #remove db test file
+        os.remove(db_file)
 
 if __name__ == '__main__':
     unittest.main()
